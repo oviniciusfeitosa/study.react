723e818b7657127e79a8ff3ea3998243d2cf3a5b
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-03-04 14:23:20 -0300

    :sparkles: Create Azure OAuth POC

--- oauth/azure/README.md
+++ oauth/azure/README.md
@@ -2,7 +2,13 @@
 
 Prove of concept (POC) for OAuth Azure.
 
+## How to
+
+- Create a `.env` file at the root level 
+- Setup the `REACT_APP_AZURE_ACTIVE_DIRECTORY_APP_CLIENT_ID` environment
+
 ## References
 
 - [SPA Azure Login button SDK MSAL](https://docs.microsoft.com/en-us/azure/developer/javascript/tutorial/single-page-application-azure-login-button-sdk-msal)
 - [Microsoft Authentication Library for JavaScript (MSAL.js)](https://github.com/AzureAD/microsoft-authentication-library-for-js)
+- [Azure Portal - ActiveDirectory/RegisteredApps](https://portal.azure.com/?quickstart=True#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredApps)
--- oauth/azure/src/App.tsx
+++ oauth/azure/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import { AccountInfo } from '@azure/msal-browser';
+
+import AzureAuthenticationButton from './azure/azure-authentication-component';
 
 function App() {
+  // current authenticated user
+  const [currentUser, setCurrentUser] = useState<AccountInfo>();
+
+  // authentication callback
+  const onAuthenticated = async (userAccountInfo: AccountInfo) => {
+    setCurrentUser(userAccountInfo);
+  };
+
+  // Render JSON data in readable format
+  const PrettyPrintJson = ({ data }: any) => {
+    return (
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    );
+  };
+
+  // Quick link - user revokes app's permission
+  const ShowPermissionRevokeLinks = () => {
+    return (
+      <div>
+        <div><a href="https://myapps.microsoft.com" target="_blank" rel="noopener">Revoke AAD permission</a></div>
+        <div><a href="https://account.live.com/consent/manage" target="_blank" rel="noopener">Revoke Consumer permission</a></div>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <h2>Microsoft Login Button application</h2>
+      <AzureAuthenticationButton onAuthenticated={onAuthenticated} />
+      {currentUser && (
+        <div>
+          <PrettyPrintJson data={currentUser} />
+          <ShowPermissionRevokeLinks />
+        </div>
+      )}
     </div>
   );
 }
