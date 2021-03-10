a6783a4dc519494b31390f56b8da516a15fc504d
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-03-10 14:44:07 -0300

    :alien: Update APP

--- crud/sample-1/src/App.js
+++ crud/sample-1/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import {
+  CssBaseline,
+  withStyles,
+} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AppHeader from './components/AppHeader';
+import Home from './pages/Home';
 
-export default App;
+const styles = theme => ({
+  main: {
+    padding: theme.spacing(3),
+    [theme.breakpoints.down('xs')]: {
+      padding: theme.spacing(2),
+    },
+  },
+});
+
+const App = ({ classes }) => (
+  <Fragment>
+    <CssBaseline />
+    <AppHeader />
+    <main className={classes.main}>
+      <Home />
+    </main>
+  </Fragment>
+);
+
+export default withStyles(styles)(App);
