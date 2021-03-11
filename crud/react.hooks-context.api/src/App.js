6c9adfef0995c45ccb25d3312fd66450251a14a3
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-03-11 17:04:58 -0300

    ✨ Bind routes

--- crud/react.hooks-context.api/src/App.js
+++ crud/react.hooks-context.api/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './stylesheet/styles.css';
+import { Home } from './components/Home';
+import { AddEmployee } from './components/Addemployee';
+import { EditEmployee } from './components/Editemployee';
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  eturn (
+    <GlobalProvider>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/add" component={AddEmployee} exact />
+        <Route path="/edit/:id" component={EditEmployee} exact />
+      </Switch>
+    </GlobalProvider>
   );
 }
 
