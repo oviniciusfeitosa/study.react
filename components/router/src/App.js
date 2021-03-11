1426111ec07d21858a5cfbfc27637bf9e65c1b9a
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-03-11 14:50:48 -0300

    ✨ Define "/" and "/about" routes

--- components/router/src/App.js
+++ components/router/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/About" component={AboutPage}/>
+    </BrowserRouter>
   );
 }
 
