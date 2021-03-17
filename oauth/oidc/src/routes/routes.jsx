1dd0fc793bced245047642263b75ab9c8a3aee72
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-03-17 01:09:01 -0300

    ⏪ ignore register route

--- oauth/oidc/src/routes/routes.jsx
+++ oauth/oidc/src/routes/routes.jsx
@@ -5,7 +5,7 @@ import { Callback } from "../components/auth/callback";
 import { Logout } from "../components/auth/logout";
 import { LogoutCallback } from "../components/auth/logoutCallback";
 import { PrivateRoute } from "./privateRoute";
-import { Register } from "../components/auth/register";
+// import { Register } from "../components/auth/register";
 import { SilentRenew } from "../components/auth/silentRenew";
 import { PublicPage } from "../components/publicPage";
 import { PrivatePage } from "../components/privatePage";
@@ -15,7 +15,7 @@ export const Routes = (
     <Route exact={true} path="/signin-oidc" component={Callback} />
     <Route exact={true} path="/logout" component={Logout} />
     <Route exact={true} path="/logout/callback" component={LogoutCallback} />
-    <Route exact={true} path="/register" component={Register} />
+    {/* <Route exact={true} path="/register" component={Register} /> */}
     <Route exact={true} path="/silentrenew" component={SilentRenew} />
     <PrivateRoute path="/dashboard" component={PrivatePage} />
     <Route path="/" component={PublicPage} />
